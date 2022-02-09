7b481e1a45578d104a2ce6d3d01a4d9191ea6563
Don Chatelain <djchatelain@gmail.com>
2022-02-08 18:22:05 -0700

    comments and improvements

--- test.js
+++ test.js
@@ -1,18 +1,24 @@
 import { compareArrays } from "./util.js";
 import 'colors';
 
+
+/**
+ * helps easily run a test
+ * The "test" function's first argument is an object containing the properties:
+ *   fn : the function you want to test
+ *   expected : the data you expect your function to return and compare to. Some datatypes may not be supported currently
+ * The second argument is the input value your function accepts. This can be any number of arguments
+ */
+
 export const test = (opts, ...args) => {
   const {
     fn,
     expected,
-    // arrayComparator = false,
   } = opts;
   const actual = fn(...args);
   let pass = false;
 
-  const arrayComparator = Array.isArray(actual)
-
-  if (arrayComparator === true) {
+  if (Array.isArray(actual)) {
     pass = compareArrays(actual, expected);
   } else {
     pass = actual === expected;
